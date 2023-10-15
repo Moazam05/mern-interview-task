bb277f999e5f868d8b50f64718a65a628d913c4d
Moazam05 <salmanmoazam08@gmail.com>
2023-10-15 21:44:00 +0500

    with auth

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 
 import { NextPage } from "next";
 import Dashboard from "../src/views/Dashboard";
+import withAuth from "../auth/withAuth";
 
 const DashboardPage: NextPage = () => {
 	return <Dashboard />;
 };
 
-export default DashboardPage;
+export default withAuth(DashboardPage);
---
+++ client/auth/withAuth.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const withAuth = (Component: any) => {
+	const AuthComponent = (props: any) => {
+		const router = useRouter();
+
+		// Check if the user has a valid token, if not, redirect to login
+		if (typeof window !== undefined) {
+			if (!localStorage.getItem("user")) {
+				router.push("/login");
+				return null;
+			}
+
+			return <Component {...props} />;
+		}
+	};
+
+	return AuthComponent;
+};
+
+export default withAuth;
